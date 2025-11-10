29c396e2ebf1cfbc4a768fe40da7ba0a1b46a8c0
Tomas <tomaslonnblad@gmail.com>
2025-11-10 11:40:43 +0100

    Update HatModel.jsx

--- src/components/models/HatModel.jsx
+++ src/components/models/HatModel.jsx
@@ -11,7 +11,7 @@ export function Model(props) {
   
 
   <group {...props} dispose={null}>
-      <group rotation={[-Math.PI / 2, 0, 0]}>
+      <group rotation={[-Math.PI / 2, 10, 0]}>
      
         <mesh
           castShadow
